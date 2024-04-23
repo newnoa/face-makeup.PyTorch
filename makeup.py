28c41ba880bb6043132971aeac7bc5d0488d86bd
chenyq_outsourcing <newnoa@aliyun.com>
2024-04-23 17:35:49 +0800

    .

--- makeup.py
+++ makeup.py
@@ -64,6 +64,14 @@ if __name__ == '__main__':
     # 12 upper lip
     # 13 lower lip
     # 17 hair
+    
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print('There are %d GPU(s) available.' % torch.cuda.device_count())
+        print('We will use the GPU:', torch.cuda.get_device_name(0))
+    else:
+        print('No GPU available, using the CPU instead.')
+        device = torch.device("cpu")
 
     args = parse_args()
 
